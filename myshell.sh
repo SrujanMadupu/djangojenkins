74b292753140dbe1deb46b5012a0bff0d40742b4
Srujan Kumar <srujan.madupu@gmail.com>
2019-05-23 18:41:08 +0530

    add mysql install code

--- myshell.sh
+++ myshell.sh
@@ -2,6 +2,8 @@
 rootpath=/root/home/srujan/dj
 yum -y install python36
 yum -y install python36-pip
+yum install mysql-server
+systemctl start mysqld
 python3 -m venv  $rootpath/jenk
 source $rootpath/jenk/bin/activate
 pip3 install -r $rootpath/requirements.txt
