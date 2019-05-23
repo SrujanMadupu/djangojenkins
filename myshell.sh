10ce1bd25dfca675bf5e3df878944ad4a8777cdc
Srujan Kumar <srujan.madupu@gmail.com>
2019-05-23 18:20:08 +0530

    changes in myshell

--- myshell.sh
+++ myshell.sh
@@ -5,4 +5,5 @@ yum -y install python36-pip
 python3 -m venv  $rootpath/jenk
 source $rootpath/jenk/bin/activate
 pip3 install -r $rootpath/requirements.txt
-python3 manage.py runserver
+python3 $rootpath/manage.py runserver
+deactivate
