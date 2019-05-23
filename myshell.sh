21e55842d2484f2fae6578532f1e30d45d78d2a6
Srujan Kumar <srujan.madupu@gmail.com>
2019-05-23 19:10:51 +0530

    changes in myshell

--- myshell.sh
+++ myshell.sh
@@ -5,10 +5,11 @@ yum -y install python36-pip
 wget https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm
 md5sum mysql80-community-release-el7-3.noarch.rpm
 rpm -ivh mysql80-community-release-el7-3.noarch.rpm
-yum -y install mysql-server
+yum -y --nogpgcheck install mysql-server
 systemctl start mysqld
 python3 -m venv  $rootpath/jenk
 source $rootpath/jenk/bin/activate
 pip3 install -r $rootpath/requirements.txt
 python3 $rootpath/manage.py runserver
 deactivate
+
