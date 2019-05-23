e50c7ad0276ca943026d2ed69e57d605c706f7f4
Srujan Kumar <srujan.madupu@gmail.com>
2019-05-23 18:48:18 +0530

    changes in myshell.sh

--- myshell.sh
+++ myshell.sh
@@ -2,6 +2,9 @@
 rootpath=/root/home/srujan/dj
 yum -y install python36
 yum -y install python36-pip
+wget https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm
+md5sum mysql80-community-release-el7-3.noarch.rpm
+rpm -ivh mysql80-community-release-el7-3.noarch.rpm
 yum install mysql-server
 systemctl start mysqld
 python3 -m venv  $rootpath/jenk
