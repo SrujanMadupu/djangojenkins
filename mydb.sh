537054f5aad411043e20d6a6ed2001789af4215c
Srujan Kumar <srujan.madupu@gmail.com>
2019-05-24 14:59:51 +0530

    changes in mydb.sh

--- mydb.sh
+++ mydb.sh
@@ -5,5 +5,7 @@ MYSQL_ROOT="root"
 DATABASE_NAME="jendev"
 
 #mysql -u root -p ${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE ${MYSQL_DATABASE}"
-mysql_secure_installation <<$MYSQL_ROOT_PASSWORD 
+mysql_secure_installation<<$MYSQL_ROOT_PASSWORD
+echo $? 
 mysql -u $MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE $DATABASE_NAME"
+echo $?
