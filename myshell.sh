7a152fac12932fb5f2cba7e1d77fad56b2a4fc6c
Srujan Kumar <srujan.madupu@gmail.com>
2019-05-27 14:47:39 +0530

    changes in myshell.sh

--- myshell.sh
+++ myshell.sh
@@ -8,43 +8,45 @@ MYSQL_ROOT='root'
 MYSQL_ROOT_PASSWORD='Mobigo#123'
 MYSQL_DATABASE='dummy'
 # MySQL Setup
-#wget https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm
-#md5sum mysql80-community-release-el7-3.noarch.rpm
-#rpm -ivh mysql80-community-release-el7-3.noarch.rpm
-#yum -y --nogpgcheck install mysql-server
+wget https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm
+md5sum mysql80-community-release-el7-3.noarch.rpm
+rpm -ivh mysql80-community-release-el7-3.noarch.rpm
+yum -y --nogpgcheck install mysql-server
 #service mysqld start
 systemctl start mysqld
-temppassword=cat /var/log/mysqld.log | grep "temporary password"|rev|cut -d: -f1|rev|sed 's/ //g' | tail -1
+tempPassword=`cat /var/log/mysqld.log | grep "temporary password"|rev|cut -d: -f1|rev|sed 's/ //g' | tail -1`
+export<<END
 spawn passwd mysql_secure_installation
 expect "Enter password for user root:"
-send $temppassword
+send $temppassword\r
 expect "New password:"
-send $MYSQL_ROOT_PASSWORD
+send $MYSQL_ROOT_PASSWORD\r
 expect "Re-enter new password:"  
-send $MYSQL_ROOT_PASSWORD
+send $MYSQL_ROOT_PASSWORD\r
 expect "Change the password for root ?"
-send "N"
+send "N\r"
 expect "Remove anonymous users? (Press y|Y for Yes, any other key for No) :"
-send "Y"
+send "Y\r"
 expect "Disallow root login remotely? (Press y|Y for Yes, any other key for No) :"
-send "Y"
+send "Y\r"
 expect "Remove test database and access to it? (Press y|Y for Yes, any other key for No) :"
-send "Y"
+send "Y\r"
 expect "Reload privilege tables now? (Press y|Y for Yes, any other key for No) :"
-send "Y"
+send "Y\r"
+END
 echo $?
 
 echo "done with mysql installation and root user setup"
 echo "creating database dummy"
 mysql -u $MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE $MYSQL_DATABASE"
 echo $?
-#echo "creting virtual environment"
-#python3 -m venv  $rootpath/jenk
-#echo $?
-#echo "Activating virtual env"
-#source $rootpath/jenk/bin/activate
-#echo $?
-#pip3 install -r $rootpath/requirements.txt
-#python3 $rootpath/manage.py runserver
-#deactivate
+echo "creting virtual environment"
+python3 -m venv  $rootpath/jenk
+echo $?
+echo "Activating virtual env"
+source $rootpath/jenk/bin/activate
+echo $?
+pip3 install -r $rootpath/requirements.txt
+python3 $rootpath/manage.py runserver
+deactivate
 
