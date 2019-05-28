d12fd47c17a28e10a3baa62174bc51de02fbc541
Srujan Kumar <srujan.madupu@gmail.com>
2019-05-28 10:30:19 +0530

    changes in myshell

--- myshell.sh
+++ myshell.sh
@@ -4,15 +4,18 @@ rootpath=/root/home/srujan/dj
 yum -y install python36
 yum -y install python36-pip
 yum -y install python36-devel
+yum -y install expect
+yum -y install tcl
 
 MYSQL_ROOT='root'
 MYSQL_ROOT_PASSWORD='Mobigo#123'
 MYSQL_DATABASE='djjenkins'
+
 # MySQL Setup
-#wget https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm
-#md5sum mysql80-community-release-el7-3.noarch.rpm
-#rpm -ivh mysql80-community-release-el7-3.noarch.rpm
-#yum -y --nogpgcheck install mysql-server
+wget https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm
+md5sum mysql80-community-release-el7-3.noarch.rpm
+rpm -ivh mysql80-community-release-el7-3.noarch.rpm
+yum -y --nogpgcheck install mysql-server
 #service mysqld start
 echo "starting mysql server"
 sleep 5
@@ -20,27 +23,26 @@ systemctl start mysqld
 tempPassword=`cat /var/log/mysqld.log | grep "temporary password"|rev|cut -d ' ' -f1|rev|sed 's/ //g' | tail -1`
 #`cat /var/log/mysqld.log | grep "temporary password"|rev|cut -d: -f1|rev|sed 's/ //g' | tail -1`
 echo $tempPassword
-mysqladmin -u root -p'$tempPassword' password '$MYSQL_ROOT_PASSWORD'
-#export<<END
-#spawn passwd 
-#mysql_secure_installation
-#expect "Enter password for user root:"
-#send $tempPassword\r
-#expect "New password:"
-#send $MYSQL_ROOT_PASSWORD\r
-#expect "Re-enter new password:"  
-#send $MYSQL_ROOT_PASSWORD\r
-#expect "Change the password for root ?"
-#send "N\r"
-#expect "Remove anonymous users? (Press y|Y for Yes, any other key for No) :"
-#send "Y\r"
-#expect "Disallow root login remotely? (Press y|Y for Yes, any other key for No) :"
-#send "Y\r"
-#expect "Remove test database and access to it? (Press y|Y for Yes, any other key for No) :"
-#send "Y\r"
-#expect "Reload privilege tables now? (Press y|Y for Yes, any other key for No) :"
-#send "Y\r"
-#END
+expect<<END
+set timeout -1
+spawn mysql_secure_installation
+expect "Enter password for user root:"
+send $tempPassword\r
+expect "New password:"
+send $MYSQL_ROOT_PASSWORD\r
+expect "Re-enter new password:"  
+send $MYSQL_ROOT_PASSWORD\r
+expect "Change the password for root ?"
+send "N\r"
+expect "Remove anonymous users? (Press y|Y for Yes, any other key for No) :"
+send "Y\r"
+expect "Disallow root login remotely? (Press y|Y for Yes, any other key for No) :"
+send "Y\r"
+expect "Remove test database and access to it? (Press y|Y for Yes, any other key for No) :"
+send "Y\r"
+expect "Reload privilege tables now? (Press y|Y for Yes, any other key for No) :"
+send "Y\r"
+END
 echo $?
 
 echo "done with mysql installation and root user setup"
