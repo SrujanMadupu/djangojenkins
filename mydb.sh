606adb8765e51d78756200013a91159b5239cd1c
Srujan Kumar <srujan.madupu@gmail.com>
2019-05-28 13:44:54 +0530

    sdfkjsd

--- mydb.sh
+++ mydb.sh
@@ -1,9 +1,13 @@
 #! /bin/bash
 MYSQL_ROOT_PASSWORD="ramesh123"
 MYSQL_DATABASE="dummydb"
-MYSQL_ROOT="root"
+SQL_ROOT="root"
+
 
 #mysql -u root -p ${MYSQL_ROOT_PASSWORD} -e "CREATE DATABASE ${MYSQL_DATABASE}"
+expect<<END
+spawn passwd $mysql_secure_installation
+expect
 mysql_secure_installation<<$MYSQL_ROOT_PASSWORD
 echo $? 
 mysql -u $MYSQL_ROOT -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE $MYSQL_DATABASE"
